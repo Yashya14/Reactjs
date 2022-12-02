e5d86799185ff9ba4c4ed098585f3bfe82cb1145
yashya <ypal23808@gmail.com>
2022-12-02 19:16:37 +0530

    UI

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import {Component} from "react";
+
+export class Home extends Component{
+    render(){
+        return(
+            <div className="container">
+                <h2>Welcome to Home Page</h2>
+            </div>
+        );
+    }
+}
+
+export default Home;
